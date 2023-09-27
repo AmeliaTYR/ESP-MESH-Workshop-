0038369bb7481572663494a4378ade1cf9e456f9
HoWeiHao <howeihao3105@gmail.com>
2023-09-27 22:55:41 +0800

    Add documents

---
+++ mesh_sensor_code/mesh_sensor_code.ino
@@ -0,0 +1,79 @@
+#include "painlessMesh.h"
+#include "Wire.h" 
+#include "SparkFunTMP102.h" 
+
+#define   MESH_PREFIX     "ESP32S3"
+#define   MESH_PASSWORD   "MeshWksh" // minimum 8 letters
+#define   MESH_PORT       5555
+
+#define SDA0_Pin 8   // select ESP32  I2C pins
+#define SCL0_Pin 9
+
+TMP102 sensor0;
+Scheduler userScheduler; // to control your personal task
+painlessMesh  mesh;
+
+// User stub
+void sendMessage() ; // Prototype so PlatformIO doesn't complain
+
+Task taskSendMessage( TASK_SECOND * 1 , TASK_FOREVER, &sendMessage );
+
+void sendMessage() {
+
+  String msg = "My temperature is ";
+  msg += sensor0.readTempC();
+  msg += "C\n";
+  Serial.print(msg);
+  mesh.sendBroadcast( msg );
+  taskSendMessage.setInterval( random( TASK_SECOND * 1, TASK_SECOND * 5 ));
+}
+
+// Needed for painless library
+void receivedCallback( uint32_t from, String &msg ) {
+  Serial.printf("startHere: Received from %u msg=%s\n", from, msg.c_str());
+}
+
+void newConnectionCallback(uint32_t nodeId) {
+    Serial.printf("--> startHere: New Connection, nodeId = %u\n", nodeId);
+}
+
+void changedConnectionCallback() {
+  Serial.printf("Changed connections\n");
+}
+
+void nodeTimeAdjustedCallback(int32_t offset) {
+    Serial.printf("Adjusted time %u. Offset = %d\n", mesh.getNodeTime(),offset);
+}
+
+void setup() {
+  Serial.begin(115200);
+  Wire.begin(SDA0_Pin, SCL0_Pin);
+
+    while(!sensor0.begin())
+  {
+    Serial.println("Cannot connect to TMP102.");
+    Serial.println("Is the board connected? Is the device ID correct?");
+    //while(1);
+  }
+
+  Serial.println("Connected to TMP102!");
+  sensor0.wakeup();
+  delay(100);
+
+//mesh.setDebugMsgTypes( ERROR | MESH_STATUS | CONNECTION | SYNC | COMMUNICATION | GENERAL | MSG_TYPES | REMOTE ); // all types on
+  mesh.setDebugMsgTypes( ERROR | STARTUP );  // set before init() so that you can see startup messages
+
+  mesh.init( MESH_PREFIX, MESH_PASSWORD, &userScheduler, MESH_PORT );
+  mesh.onReceive(&receivedCallback);
+  mesh.onNewConnection(&newConnectionCallback);
+  mesh.onChangedConnections(&changedConnectionCallback);
+  mesh.onNodeTimeAdjusted(&nodeTimeAdjustedCallback);
+
+  userScheduler.addTask( taskSendMessage );
+  taskSendMessage.enable();
+}
+
+void loop() {
+  // it will run the user scheduler as well
+  mesh.update();
+}
